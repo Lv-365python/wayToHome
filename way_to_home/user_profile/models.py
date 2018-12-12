3902e64ecd68cfc620a34491e4f7cc19a90333a5
Orest <Borovyorest@gmail.com>
2018-12-12 21:13:37 +0200

    Create model for user profile entity

--- way_to_home/user_profile/models.py
+++ way_to_home/user_profile/models.py
@@ -1,1 +1,53 @@
 """This module implements class that represents the user profile entity."""
+
+from user.models import User
+from django.db import models, IntegrityError
+from utils.abstract_models import AbstractModel
+
+
+class UserProfile(AbstractModel):
+    """Model for user profile entity."""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        """Method that returns route instance as string."""
+
+        return f'{self.first_name} {self.last_name}'
+
+    def to_dict(self):
+        """Method that returns dict with object's attributes."""
+
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'user_id': self.user.id
+        }
+
+    @classmethod
+    def create(cls, user, first_name=None, last_name=None):  # pylint: disable=arguments-differ
+        """Method for object creation."""
+
+        user_profile = cls()
+        user_profile.user = user
+        user_profile.first_name = first_name
+        user_profile.last_name = last_name
+
+        try:
+            user_profile.save()
+        except (ValueError, IntegrityError):
+            user_profile = None
+
+        return user_profile
+
+    def update(self, first_name=None, last_name=None):  # pylint: disable=arguments-differ
+        """Method for object updating."""
+
+        if first_name:
+            self.first_name = first_name
+        if last_name:
+            self.last_name = last_name
+
+        self.save()
