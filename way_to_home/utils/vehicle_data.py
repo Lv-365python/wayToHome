16ed99b076485a937ec2e02a1ee7f547b8fdfd2c
Sasha1152 <saxin@ukr.net>
2018-12-17 00:40:01 +0200

    fix the code to satisfy 'travis'

--- way_to_home/utils/vehicle_data.py
+++ way_to_home/utils/vehicle_data.py
@@ -1,20 +1,25 @@
+"""
+    This module use for parsing data got in real time from 'Easy Way' source
+    in GTFS format.
+"""
+
 import google.protobuf
 from google.transit import gtfs_realtime_pb2
 import requests
 
 
-feed = gtfs_realtime_pb2.FeedMessage()
-response = requests.get('http://track.ua-gis.com/gtfs/lviv/vehicle_position')
-feed.ParseFromString(response.content)
+FEED = gtfs_realtime_pb2.FeedMessage()
+RESPONSE = requests.get('http://track.ua-gis.com/gtfs/lviv/vehicle_position')
+FEED.ParseFromString(RESPONSE.content)
 
 
 def get_vehicle_data(vehicle_id):
     """
-    This function takes certain vehicle id as parameter and
-    returns appropriate data about it's location, speed etc.
+        This function takes certain vehicle id as parameter and
+        returns appropriate data about it's location, speed etc.
     """
     vehicle_data_json = {}
-    for entity in feed.entity:
+    for entity in FEED.entity:
         if entity.vehicle.vehicle.id == vehicle_id:
             vehicle_data_json['route_id'] = entity.vehicle.trip.route_id
             vehicle_data_json['lat'] = entity.vehicle.position.latitude
