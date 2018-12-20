f57c92475f59f2e7b16ced93a7cf33a9bbea8543
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-12-20 14:18:09 +0200

    create func that provides downloading files

--- way_to_home/utils/file_handlers.py
+++ way_to_home/utils/file_handlers.py
@@ -1,1 +1,17 @@
 """This module implements helpers functions for work with files."""
+
+import requests
+
+
+def load_file(url, save_to='./'):
+    """Download file from `url` to directory with path `save_to`."""
+    file_name = url.split('/')[-1]
+
+    request = requests.get(url)
+    if not request.status_code == 200:
+        return False
+
+    with open(f'{save_to}/{file_name}', 'wb') as file:
+        file.write(request.content)
+
+    return True
