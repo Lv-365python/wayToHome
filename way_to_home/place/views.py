7b3f027abcec7f3be1679e1fb70ff07fd064c321
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-12-20 21:38:53 +0200

    create view for retrieve place object

--- way_to_home/place/views.py
+++ way_to_home/place/views.py
@@ -24,3 +24,24 @@ class PlaceView(View):
 
         place = place.to_dict()
         return JsonResponse(place, status=201)
+
+    def get(self, request, obj_id=None):
+        """Handle the request for retrieve a place object or user`s places."""
+        user = request.user
+
+        if not obj_id:
+            places = user.places.all()
+            data = [place.to_dict() for place in places]
+
+            return JsonResponse(data, status=200, safe=False)
+
+        place = Place.get_by_id(obj_id)
+
+        if not place:
+            return HttpResponse('object not found', status=404)
+
+        if place.user_id and place.user_id != user.id:
+            return HttpResponse('access denied for non-owner users', status=403)
+
+        place = place.to_dict()
+        return JsonResponse(place, status=200)
