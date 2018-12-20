a056b4ecb16b4ad024a4a1f026501dab26305e23
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-12-20 20:21:40 +0200

    create urls module for place app

---
+++ way_to_home/place/urls.py
@@ -0,0 +1,10 @@
+"""The module that provide URL configuration for place app."""
+
+from django.urls import re_path
+
+from .views import PlaceView
+
+
+urlpatterns = [
+    re_path('place/(?P<id>\d+)?', PlaceView.as_view(), name='place'),
+]
