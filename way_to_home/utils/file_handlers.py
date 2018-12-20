ac298787b4ebe9298ef42cdab9c277149dfa3ba0
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-12-20 14:23:26 +0200

    create parser for file with csv format

--- way_to_home/utils/file_handlers.py
+++ way_to_home/utils/file_handlers.py
@@ -2,6 +2,7 @@
 
 import requests
 import pickle
+import csv
 
 
 def _get_fields_position(row_header, required_fields):
@@ -28,6 +29,26 @@ def load_file(url, save_to='./'):
         file.write(request.content)
 
     return True
+
+
+def parse_csv_file(path_to_file, required_fields):
+    """Provide csv file parsing with the required fields."""
+    parsed_data = []
+
+    with open(path_to_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        for row in csv_reader:
+            if csv_reader.line_num == 1:
+                fields_position = _get_fields_position(row, required_fields)
+
+            item = {}
+            for field, position in fields_position.items():
+                item[field] = row[position]
+
+            parsed_data.append(item)
+
+    return parsed_data
 
 
 def pickle_data(data, path_to_file):
