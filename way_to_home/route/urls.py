042f4c0cde3426ee13c739375204291829f449d4
Dimitri <dimi_gor@icloud.com>
2018-12-23 15:49:48 +0200

    add urls file, fix route methods

---
+++ way_to_home/route/urls.py
@@ -0,0 +1,10 @@
+"""The module that provide URL configuration for route app."""
+
+from django.urls import re_path
+
+from .views import RouteView
+
+
+urlpatterns = [
+    re_path('(?P<route_id>\d+)?', RouteView.as_view(), name='route'),
+]
--- way_to_home/route/views.py
+++ way_to_home/route/views.py
@@ -17,8 +17,20 @@ class RouteView(View):
     operations with route model.
     """
 
+    http_method_names = ['get']
+
     def get(self, request, route_id=None):
         """ Method that handles GET request. """
+        if not request.user:
+            return HttpResponse('permission denied', status=400)
+
+        if not route_id:
+            # TODO get all user routes
+            routes = Route.objects.all()
+
+            data = [route.to_dict() for route in routes]
+
+            return JsonResponse(data, status=200, safe=False)
 
         route = Route.get_by_id(obj_id=route_id)
         if not route:
@@ -26,18 +38,19 @@ class RouteView(View):
 
         return JsonResponse(route.to_dict(), status=200)
 
-    def put(self, request, route_id=None):
+    def put(self, request, route_id=None):  # pylint: disable=R0201
         """ Method that handles PUT request. """
 
+        if not request.user:
+            return HttpResponse('permission denied', status=400)
+
         route = Route.get_by_id(obj_id=route_id)
-
-        data = request.body
-
-        if not data:
-            return HttpResponse('database operation is failed, data not found', status=404)
-
         if not route:
             return HttpResponse('database operation is failed, route not found', status=404)
+
+        data = request.body
+        if not data:
+            return HttpResponse('database operation is failed, data not found', status=404)
 
         data = {
             'time': data.get('time'),
@@ -48,15 +61,18 @@ class RouteView(View):
             'end_place': data.get('end_place'),
         }
 
-        route.update(**data)
+        is_updated = route.update(**data)
+        if not is_updated:
+            return HttpResponse('database was not updated', status=400)
 
         return HttpResponse('database is updated', status=200)
 
     def post(self, request):
         """ Method that handles POST request. """
+        if not request.user:
+            return HttpResponse('permission denied', status=400)
 
         data = request.body
-
         if not data:
             return HttpResponse('database operation is failed, invalid data', status=400)
 
@@ -72,16 +88,16 @@ class RouteView(View):
         route = Route.create(**data)
 
         if route:
-            route = route.to_dict()
-            return JsonResponse(route, status=201)
+            return JsonResponse(route.to_dict(), status=201)
 
         return HttpResponse('database operation in failed', status=400)
 
-    def delete(self, route_id=None):
+    def delete(self, request, route_id=None):  # pylint: disable=R0201
         """ Method that handles DELETE request. """
+        if not request.user:
+            return HttpResponse('permission denied', status=400)
 
         route = Route.get_by_id(obj_id=route_id)
-
         if not route:
             return HttpResponse('database operation is failed, route not found', status=404)
 
