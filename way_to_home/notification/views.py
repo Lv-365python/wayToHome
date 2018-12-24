b21a0650518ed1a6094909fa5075f45be89c136c
Dimitri <dimi_gor@icloud.com>
2018-12-24 15:34:40 +0200

    check user permission for CRUD operations

--- way_to_home/notification/views.py
+++ way_to_home/notification/views.py
@@ -9,6 +9,7 @@ from django.views import View
 from django.http import HttpResponse, JsonResponse
 
 from notification.models import Notification
+from way.models import Way
 
 
 class NotificationView(View):
@@ -18,14 +19,17 @@ class NotificationView(View):
     """
     def get(self, request, notification_id=None):
         """ Method that handles GET request. """
-        if not request.user:
+        user = request.user
+        if not user:
             return HttpResponse('login to view notifications', status=400)
 
         if not notification_id:
-            # TODO get all user notifications
-            notifications = Notification.objects.all()
 
-            data = [notification.to_dict() for notification in notifications]
+            data = []
+            for way in user.ways.all():
+                for notific in Notification.objects.all():
+                    if way == notific.way:
+                        data.append(notific.to_dict())
 
             return JsonResponse(data, status=200, safe=False)
 
@@ -45,11 +49,15 @@ class NotificationView(View):
         if not data:
             return HttpResponse('database operation is failed, data not found', status=404)
 
+        user = request.user
+        if not user:
+            return HttpResponse('permission denied', status=400)
+
         notification = Notification.get_by_id(notification_id)
         if not notification:
             return HttpResponse('database operation is failed, notification not found', status=404)
 
-        if not request.user:
+        if user != notification.way.user:
             return HttpResponse('permission denied', status=400)
 
         data = {
@@ -63,14 +71,14 @@ class NotificationView(View):
         # if not notification_data_validator(data, update=True):
         #     return HttpResponse('invalid data', status=400)
 
-        is_updated = Notification.update(**data)
+        is_updated = notification.update(**data)
 
         if not is_updated:
             return HttpResponse('Notification is not updated', status=400)
 
         return HttpResponse('database is updated', status=200)
 
-    def post(self, request):
+    def post(self, request, notification_id=None):
         """ Method that handles POST request. """
         if not request.user:
             return HttpResponse('permission denied', status=400)
@@ -84,7 +92,7 @@ class NotificationView(View):
             'end_time': data.get('end_time'),
             'week_day': data.get('week_day'),
             'time': data.get('time'),
-            'way': data.get('way')
+            'way': Way.get_by_id(obj_id=data.get('way'))
         }
 
         # if not notification_data_validator(data):
@@ -99,15 +107,16 @@ class NotificationView(View):
 
     def delete(self, request, notification_id=None):  # pylint: disable=R0201
         """ Method that handles DELETE request. """
-        if not request.user:
+        user = request.user
+        if not user:
             return HttpResponse('permission denied', status=400)
-
-        if not notification_id:
-            return HttpResponse('object id is not received', status=400)
 
         notification = Notification.get_by_id(notification_id)
         if not notification:
             return HttpResponse('database operation is failed, notification not found', status=404)
+
+        if user != notification.way.user:
+            return HttpResponse('permission denied', status=400)
 
         is_deleted = Notification.delete_by_id(notification_id)
         if not is_deleted:
