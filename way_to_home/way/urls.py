bf6f4e6e6e6a4f2fb800f3a53a1826926deb3ee3
Max <mrbrainnygg@gmail.com>
2018-12-25 14:49:15 +0200

    updated urls for way app

--- way_to_home/way/urls.py
+++ way_to_home/way/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, re_path
 from way.views import WayView
 
 urlpatterns = [
-    re_path('(?P<way_id>\d+)?', WayView.as_view(), name='way'),
+    re_path('(?P<way_id>\d+)?/$', WayView.as_view(), name='way'),
 ]
