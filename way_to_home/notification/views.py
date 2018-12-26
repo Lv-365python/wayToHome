0828727530843932f9e4ff40bd1af588c73f53b5
Dimitri <dimi_gor@icloud.com>
2018-12-26 12:08:18 +0200

    get object by filter().first()

--- way_to_home/notification/views.py
+++ way_to_home/notification/views.py
@@ -20,26 +20,26 @@ class NotificationView(View):
     def get(self, request, way_id, notification_id=None):
         """ Method that handles GET request. """
         user = request.user
-        way = user.ways.filter(id=way_id)
+        way = user.ways.filter(id=way_id).first()
 
         if not way:
             return HttpResponse('way is not found', status=400)
 
         if not notification_id:
-            data = [notification.to_dict() for notification in way[0].notifications.all().order_by('week_day')]
+            data = [notification.to_dict() for notification in way.notifications.all().order_by('week_day')]
             return JsonResponse(data, status=200, safe=False)
 
-        notification = way[0].notifications.filter(id=notification_id)
+        notification = way.notifications.filter(id=notification_id).first()
         if not notification:
             return HttpResponse('Notification not found', status=400)
 
-        return JsonResponse(notification[0].to_dict(), status=200)
+        return JsonResponse(notification.to_dict(), status=200)
 
     def put(self, request, way_id, notification_id=None):  # pylint: disable=R0201
         """ Method that handles PUT request. """
         user = request.user
         data = request.body
-        way = user.ways.filter(id=way_id)
+        way = user.ways.filter(id=way_id).first()
 
         if not way:
             return HttpResponse('failed, way not found', status=400)
@@ -47,7 +47,7 @@ class NotificationView(View):
         if not notification_id:
             return HttpResponse('notification_id is not received', status=400)
 
-        notification = way[0].notifications.filter(id=notification_id)
+        notification = way.notifications.filter(id=notification_id).first()
         if not notification:
             return HttpResponse('failed, notification not found', status=400)
 
@@ -62,7 +62,7 @@ class NotificationView(View):
         # if not notification_data_validator(data, update=True):
         #     return HttpResponse('invalid data', status=400)
 
-        is_updated = notification[0].update(**data)
+        is_updated = notification.update(**data)
         if not is_updated:
             return HttpResponse('Notification is not updated', status=400)
 
@@ -72,7 +72,7 @@ class NotificationView(View):
         """ Method that handles POST request. """
         user = request.user
         data = request.body
-        way = user.ways.filter(id=way_id)
+        way = user.ways.filter(id=way_id).first()
 
         if not way:
             return HttpResponse('way is not found', status=400)
@@ -97,7 +97,7 @@ class NotificationView(View):
     def delete(self, request, way_id, notification_id=None):  # pylint: disable=R0201
         """ Method that handles DELETE request. """
         user = request.user
-        way = user.ways.filter(id=way_id)
+        way = user.ways.filter(id=way_id).first()
 
         if not way:
             return HttpResponse('failed, way not found', status=400)
@@ -105,7 +105,7 @@ class NotificationView(View):
         if not notification_id:
             return HttpResponse('obj_ids are not received', status=400)
 
-        notification = way[0].notifications.filter(id=notification_id)
+        notification = way.notifications.filter(id=notification_id).first()
         if not notification:
             return HttpResponse('failed, notification not found', status=400)
 
