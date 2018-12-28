146cc3bead5157f021c281b86673a9c9159db5af
”Viktoriia <mizin4ik3@gmail.com>
2018-12-28 12:48:19 +0200

    modify validators

--- way_to_home/utils/validators.py
+++ way_to_home/utils/validators.py
@@ -1,7 +1,7 @@
 """
 Project validators
 ==================
-Module that provides validation functions for all kinds of project's data.
+Module that provides validation functions for all kinds of project's data
 """
 import re
 from datetime import datetime, timedelta
@@ -31,7 +31,7 @@ def string_validator(value, min_length=0, max_length=None):
 
 
 def coordinate_validator(value, min_value, max_value):
-    """Provide coordinate validation."""
+    """Provide coordinate validation"""
 
     if not isinstance(value, float):
         return False
@@ -43,7 +43,7 @@ def coordinate_validator(value, min_value, max_value):
 
 
 def required_keys_validator(data, keys_required):
-    """Provide required keys validation."""
+    """Provide required keys validation"""
     keys = set(data.keys())
     keys_required = set(keys_required)
 
@@ -51,7 +51,7 @@ def required_keys_validator(data, keys_required):
 
 
 def date_validator(date):
-    """ Function that provides validation data type"""
+    """Function that provides validation data type"""
     for mask in DATE_MASK:
         try:
             date = datetime.strptime(date, mask)
@@ -60,8 +60,8 @@ def date_validator(date):
             pass
 
 
-def start_date_notification_validator(start_date):
-    """Function validates start_date field in Notification model"""
+def start_date_validator(start_date):
+    """Function validates start_date field"""
     today = datetime.now() - timedelta(days=1)
     start_date = date_validator(start_date)
     if not start_date:
@@ -72,11 +72,11 @@ def start_date_notification_validator(start_date):
     return True
 
 
-def end_date_notification_validator(end_date, start_date):
-    """Function validates end_date field in Notification model"""
+def end_date_validator(end_date, start_date):
+    """Function validates end_date field"""
     start_date = date_validator(start_date) if start_date else datetime.now() - timedelta(days=1)
     end_date = date_validator(end_date)
-    if not start_date or not end_date:
+    if not (start_date or end_date):
         return False
     if start_date > end_date:
         return False
@@ -94,18 +94,19 @@ def time_validator(time):
     return True
 
 
-def week_day_notification_validator(week_day):
-    """Function validates week_day field in Notification model"""
-    if isinstance(week_day, int):
-        if int(week_day) in range(0, 7):
-            return True
+def week_day_validator(week_day):
+    """Function validates week_day field"""
+    if not isinstance(week_day, int):
+        return False
+    if week_day not in range(0, 7):
+        return False
 
-    return False
+    return True
 
 
 def notification_data_validator(data, update=False):
     """Function that provides update notification model data validation"""
-    required_fields = ['start_date', 'end_date', 'week_day', 'time', 'way_id']
+    required_fields = ['start_date', 'end_date', 'week_day', 'time']
 
     if not update:
         if not required_keys_validator(data, required_fields):
@@ -115,17 +116,15 @@ def notification_data_validator(data, update=False):
         'start_date',
         'end_date',
         'week_day',
-        'time',
-        'way_id'
+        'time'
     ]
 
     filtered_data = {key: data.get(key) for key in notification_model_fields}
     validation_rules = {
-        'start_date': start_date_notification_validator,
-        'end_date': lambda val: end_date_notification_validator(val, data.get('start_date')),
-        'week_day': week_day_notification_validator,
+        'start_date': start_date_validator,
+        'end_date': lambda val: end_date_validator(val, data.get('start_date')),
+        'week_day': week_day_validator,
         'time': time_validator,
-        'way': lambda val: isinstance(val, int) and val > 0
     }
 
     for key, value in filtered_data.items():
@@ -137,7 +136,7 @@ def notification_data_validator(data, update=False):
 
 
 def place_data_validator(data, update=False):
-    """Provide data validation before create/update place object."""
+    """Provide data validation before create/update place object"""
     required_fields = ['longitude', 'latitude', 'address']
 
     if not update:
@@ -170,17 +169,14 @@ def place_data_validator(data, update=False):
 
 
 def route_data_validator(data, update=False):
-    """Provide data validation before create/update route object."""
-    required_fields = ['way', 'start_place', 'end_place', 'time', 'position']
+    """Provide data validation before create/update route object"""
+    required_fields = ['time', 'position']
 
     if not update:
         if not required_keys_validator(data, required_fields):
             return False
 
     route_model_fields = [
-        'way',
-        'start_place',
-        'end_place',
         'time',
         'position',
         'transport_id'
@@ -188,12 +184,9 @@ def route_data_validator(data, update=False):
 
     filtered_data = {key: data.get(key) for key in route_model_fields}
     validation_rules = {
-        'way': lambda val: isinstance(val, int) and val > 0,
-        'start_place': lambda val: isinstance(val, int) and val > 0,
-        'end_place': lambda val: isinstance(val, int) and val > 0,
         'position': lambda val: isinstance(val, int) and val >= 0,
-        'transport_id': lambda val: isinstance(val, int) and val > 0,
-        'time': time_validator,
+        'transport_id': lambda val: isinstance(val, int) and val >= 0,
+        'time': time_validator
     }
 
     for key, value in filtered_data.items():
@@ -204,32 +197,36 @@ def route_data_validator(data, update=False):
     return True
 
 
-def way_name_validator(name):
+def way_data_validator(name):
     """Function that provides way model data validation"""
-    if string_validator(name, min_length=1, max_length=128):
-        return True
-    return False
+    if not string_validator(name, min_length=1, max_length=128):
+        return False
+    return True
 
 
 def email_validator(email):
-    """Function that provides email validation."""
+    """Function that provides email validation"""
+    if not string_validator(email, max_length=64):
+        return False
     try:
         email = email.lower().strip()
         validate_email(email)
-        return True
     except (ValidationError, AttributeError):
         return False
+    return True
 
 
 def password_validator(password):
-    """Function that provides password validation."""
+    """Function that provides password validation"""
+    if not string_validator(password, max_length=128):
+        return False
     try:
         template = re.compile(PASSWORD_REG_EXP)
-        if template.match(password):
-            return True
-        return False
+        if not template.match(password):
+            return False
     except (TypeError, AttributeError):
         return False
+    return True
 
 
 def registration_validate(data):
@@ -237,19 +234,16 @@ def registration_validate(data):
     required_keys = ['email', 'password']
     if not required_keys_validator(data, required_keys):
         return False
-    if not (string_validator(data.get('email')) and email_validator(data.get('email'))):
+    if not email_validator(data.get('email')):
         return False
-    if not (string_validator(data.get('password')) and password_validator(data.get('password'))):
+    if not password_validator(data.get('password')):
         return False
     return True
 
 
 def login_validate(data):
-    """Function that provides login validation."""
-    if not data:
-        return False
-    if not required_keys_validator(data, ['email', 'password']):
-        return False
-    if not email_validator(data['email']):
+    """Function that provides login validation"""
+    required_keys = ['email', 'password']
+    if not required_keys_validator(data, required_keys):
         return False
     return True
