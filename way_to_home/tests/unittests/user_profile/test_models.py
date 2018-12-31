ab1574de6d0c65692f7c76f592ded83757f72aed
Sasha1152 <saxin@ukr.net>
2018-12-31 13:07:02 +0200

    create test for 'user_profile.models.test_to_dict' function

--- way_to_home/tests/unittests/user_profile/test_models.py
+++ way_to_home/tests/unittests/user_profile/test_models.py
@@ -14,12 +14,36 @@ class UserProfileTestCase(TestCase):
 
     def setUp(self):
         """ Method that provides preparation before testing UserProfile model's features. """
-        user = CustomUser.objects.create(id=1, email='user@mail.com', password='1111', is_active=True)
-        first_name = CustomUser.objects.create('userName')
-        last_name = CustomUser.objects.create('userSurname')
+        custom_user = CustomUser.objects.create(
+            id=1,
+            email='user@mail.com',
+            password='1111',
+            is_active=True
+        )
+        custom_user.save()
+        self.user_profile = UserProfile(
+            id=2,
+            first_name='userName',
+            last_name='userSurname',
+            user_id=custom_user.id,
+        )
+        self.user_profile.save()
 
     def test_str(self):
         """ Test of the __str__() method """
-        returned_string = str(CustomUser.objects.get(id=1))
-        expected_string = 'userName userSurname'
+        user_profile = UserProfile.get_by_id(obj_id=self.user_profile.id)
+        returned_string = str(user_profile)
+        expected_string = f'{user_profile.first_name} {user_profile.last_name}'
         self.assertEqual(returned_string, expected_string)
+
+    def test_to_dict(self):
+        """ Test for checking dictionary that 'to_dict' method return"""
+        user_profile = UserProfile.get_by_id(obj_id=self.user_profile.id)
+        returned_dict = user_profile.to_dict()
+        expected_dict = {
+            'id': self.user_profile.id,
+            'first_name': self.user_profile.first_name,
+            'last_name': self.user_profile.last_name,
+            'user_id': self.user_profile.user_id
+        }
+        self.assertDictEqual(returned_dict, expected_dict)
