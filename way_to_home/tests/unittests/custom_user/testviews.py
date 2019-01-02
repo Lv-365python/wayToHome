8943635d17383cf2ae4e553d335bb80a19917e65
Max <mrbrainnygg@gmail.com>
2019-01-03 01:10:23 +0200

    fixes in custom user views tests

--- way_to_home/tests/unittests/custom_user/testviews.py
+++ way_to_home/tests/unittests/custom_user/testviews.py
@@ -24,7 +24,7 @@ class CustomProfileViewTest(TestCase):
             phone_number='+380111111111',
             is_active=True
         )
-        self.custom_user.set_password('1111')
+        self.custom_user.set_password('1111Bb')
         self.custom_user.save()
 
         self.inactive_user = CustomUser.objects.create(
@@ -33,8 +33,8 @@ class CustomProfileViewTest(TestCase):
             google_token={'clientId': '123', 'userKey': 'testuser'},
             phone_number='+380111111111',
         )
-        self.custom_user.set_password('2222')
-        self.custom_user.save()
+        self.inactive_user.set_password('2222Bb')
+        self.inactive_user.save()
 
         self.client = Client()
 
@@ -42,7 +42,7 @@ class CustomProfileViewTest(TestCase):
         """Provides test for a (POST) request to sign up a user with correct signup data."""
         data = {
             'email': 'another@mail.com',
-            'password': '123456789A'
+            'password': '123456789aA'
         }
         url = reverse('signup')
         response = self.client.post(url, json.dumps(data), content_type='application/json')
@@ -52,7 +52,7 @@ class CustomProfileViewTest(TestCase):
         """Provides test for a (POST) request to sign up a user with taken email in request body."""
         test_data = {
             'email': 'user@mail.com',
-            'password': '123456789A'
+            'password': '123456789aA'
         }
         url = reverse('signup')
         response = self.client.post(url, json.dumps(test_data), content_type='application/json')
@@ -62,44 +62,51 @@ class CustomProfileViewTest(TestCase):
         """Provides test for a (POST) request to sign up a user with inappropriate email in request body."""
         test_data = {
             'email': 'user',
-            'password': '123456789A'
+            'password': '123456789aA'
         }
         url = reverse('signup')
         response = self.client.post(url, json.dumps(test_data), content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
-    def test_registration_confirm(self):
+    def test_sign_up_failed_password(self):
+        """Provides test for a (POST) request to sign up a user with inappropriate password in request body."""
+        test_data = {
+            'email': 'another@mail.com',
+            'password': '123'
+        }
+        url = reverse('signup')
+        response = self.client.post(url, json.dumps(test_data), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_signup_confirm(self):
         """Provides test for a (GET) request to confirm user sign up with correct token."""
         token = create_token(data={'email': self.inactive_user.email})
         url = reverse('confirm_signup', kwargs={'token': token})
         response = self.client.get(url)
-        self.assertEqual(self.inactive_user.is_active, True)
         self.assertEqual(response.status_code, 200)
 
-    def test_registration_confirm_invalid_token(self):
+    def test_signup_confirm_invalid_token(self):
         """Provides test for a (GET) request to confirm user sign up with incorrect token."""
         token = 'bad_token'
         url = reverse('confirm_signup', kwargs={'token': token})
         response = self.client.get(url)
-        self.assertEqual(self.inactive_user.is_active, False)
         self.assertEqual(response.status_code, 498)
 
-    def test_registration_confirm_invalid_email(self):
+    def test_signup_confirm_invalid_email(self):
         """Provides test for a (GET) request to confirm user sign up with token for an email that wasn't registered."""
         token = create_token(data={'email': 'random@email'})
         url = reverse('confirm_signup', kwargs={'token': token})
         response = self.client.get(url)
-        self.assertEqual(self.inactive_user.is_active, False)
-        self.assertEqual(response.status_code, 498)
+        self.assertEqual(response.status_code, 400)
 
     def test_log_in(self):
         """Provides test for a (POST) request to log in a registered user with correct credentials."""
         test_data = {
             'email': 'user@mail.com',
-            'password': '1111'
+            'password': '1111Bb'
         }
-        url = reverse('login')
-        response = self.client.get(url, json.dumps(test_data), content_type='application/json')
+        url = reverse('login_user')
+        response = self.client.post(url, json.dumps(test_data), content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
     def test_log_in_fail(self):
@@ -108,7 +115,6 @@ class CustomProfileViewTest(TestCase):
             'email': 'wrong_email',
             'password': 'random_password'
         }
-        url = reverse('login')
-        response = self.client.get(url, json.dumps(test_data), content_type='application/json')
+        url = reverse('login_user')
+        response = self.client.post(url, json.dumps(test_data), content_type='application/json')
         self.assertEqual(response.status_code, 400)
-
