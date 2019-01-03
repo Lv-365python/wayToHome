984c4e0956216fb94d63b3e218dbf7080ac86003
Max <mrbrainnygg@gmail.com>
2019-01-03 02:05:35 +0200

    added view to update user profile entity

--- way_to_home/user_profile/views.py
+++ way_to_home/user_profile/views.py
@@ -51,3 +51,34 @@ class UserProfileView(View):
             return RESPONSE_403_ACCESS_DENIED
 
         return JsonResponse(profile.to_dict, status=200)
+
+    def put(self, request, user_profile_id=None):  # pylint: disable=R0201, R0911
+        """Handle the request to update an existing user_profile object with appropriate id."""
+        user = request.user
+
+        if not user_profile_id:
+            return HttpResponse('object id is not received', status=400)
+
+        data = request.body
+
+        if not data:
+            return RESPONSE_400_EMPTY_JSON
+
+        profile = UserProfile.get_by_id(user_profile_id)
+
+        if not profile:
+            return RESPONSE_404_OBJECT_NOT_FOUND
+
+        if profile.user and profile.user != user:
+            return RESPONSE_403_ACCESS_DENIED
+
+        data = {
+            'first_name': data.get('first_name'),
+            'last_name': data.get('last_name')
+        }
+
+        is_updated = profile.update(**data)
+        if not is_updated:
+            return RESPONSE_400_DB_OPERATION_FAILED
+
+        return RESPONSE_200_UPDATED
