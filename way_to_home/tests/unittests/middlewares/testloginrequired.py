708ff3ae06f163b0f22fb16055a758648d581e09
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2019-01-04 17:57:42 +0200

    Remove senselessly tests for loginrequired

--- way_to_home/tests/unittests/middlewares/testloginrequired.py
+++ way_to_home/tests/unittests/middlewares/testloginrequired.py
@@ -1,7 +1,6 @@
 """This module provides tests for custom LoginRequired middleware."""
 
 from django.test import TestCase, Client
-from django.urls import reverse
 
 from custom_user.models import CustomUser
 from middlewares.login_required import GUESTS_PATHS
@@ -21,48 +20,31 @@ class LoginRequiredTestCase(TestCase):
         self.user_client = Client()
         self.user_client.login(email='testuser@mail.com', password='testpassword')
 
-    def test_invalid_json(self):
-        """Provide tests for PUT/POST/PATCH request with invalid JSON data."""
-        data = 'invalid data'
+    def test_post_with_invalid_json(self):
+        """Provide tests for POST request with invalid JSON data."""
+        path = '/api/'
+        invalid_json = 'invalid json'
 
-        response = self.client.post('/api/', data, content_type='application/json')
+        response = self.client.post(path, invalid_json, content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
-        response = self.client.put('/api/', data, content_type='application/json')
-        self.assertEqual(response.status_code, 400)
+    def test_put_with_invalid_json(self):
+        """Provide tests for PUT request with invalid JSON data."""
+        path = '/api/'
+        invalid_json = 'invalid json'
 
-        response = self.client.patch('/api/', data, content_type='application/json')
+        response = self.client.put(path, invalid_json, content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
     def test_forbidden_request_for_user(self):
-        """Provide tests guests path for authenticated user."""
-        data = {}
+        """Provide tests for user`s GET requests with paths that available only for guests."""
         for path in GUESTS_PATHS:
             response = self.user_client.get(path)
             self.assertEqual(response.status_code, 403)
 
-            response = self.user_client.post(path, data, content_type='application/json')
-            self.assertEqual(response.status_code, 403)
-
-            response = self.user_client.put(path, data, content_type='application/json')
-            self.assertEqual(response.status_code, 403)
-
-            response = self.user_client.patch(path, data, content_type='application/json')
-            self.assertEqual(response.status_code, 403)
-
     def test_forbidden_request_for_guest(self):
-        """Provide tests unsuccessful requests for guest."""
-        path = reverse('place')
-        data = {}
+        """Provide tests for guest`s requests with path that available only for users."""
+        path = '/api/'
 
         response = self.guest_client.get(path)
-        self.assertEqual(response.status_code, 403)
-
-        response = self.guest_client.post(path, data, content_type='application/json')
-        self.assertEqual(response.status_code, 403)
-
-        response = self.guest_client.put(path, data, content_type='application/json')
-        self.assertEqual(response.status_code, 403)
-
-        response = self.guest_client.patch(path, data, content_type='application/json')
         self.assertEqual(response.status_code, 403)
