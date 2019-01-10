b1c6a4f01d61ca2e9a1be1dfc30820fc1e2d821d
meelros <meelros91@gmail.com>
2019-01-10 14:35:15 +0200

    Add google map

---
+++ way_to_home/static/src/components/map/map.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+import './map.css'
+
+export class MapContainer extends Component {
+    render() {
+        return (
+            <Map
+                google={this.props.google}
+                zoom={14}
+                className='map'
+                initialCenter={{
+                    lat: 49.84,
+                    lng: 24.028667
+                }}
+            />
+        );
+    }
+}
+
+export default GoogleApiWrapper({
+
+    //Google Map Api you can find in telegram
+
+    apiKey: ''
+})(MapContainer);
