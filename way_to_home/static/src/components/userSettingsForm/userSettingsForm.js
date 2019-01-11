3ac753377fa6a867ff0c94f6e3299bcca7cfcfe7
meelros <meelros91@gmail.com>
2019-01-11 14:30:33 +0200

    add function that change url and div where we call it

--- way_to_home/static/src/components/userSettingsForm/userSettingsForm.js
+++ way_to_home/static/src/components/userSettingsForm/userSettingsForm.js
@@ -3,33 +3,42 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import './userSettingsForm.css';
-
+import {Link} from 'react-router-dom'
 
 class userSettingsForm extends React.Component {
-  state = {
-    value: 0,
-  };
+    state = {
+        value: 0,
+    };
 
-  handleChange = (event, value) => {
-    this.setState({ value });
-  };
+    handleChange = (event, value) => {
+        this.setState({ value });
+    };
 
-  render() {
-    const { value } = this.state;
+    routeChange = () => {
+        window.location.reload('/home')
+    }
 
-    return (
-      <div className="settingsForm">
-        <AppBar position="static">
-          <Tabs value={value} onChange={this.handleChange}>
-            <Tab label="Профіль" className="settingsTab" />
-            <Tab label="Шляхи" className="settingsTab" />
-          </Tabs>
-        </AppBar>
-          {value === 0 && <div>asdasdas</div>}
-          {value === 1 && <div>vbbcbbcccv</div>}
-      </div>
-    );
-  }
+    render() {
+        const { value } = this.state;
+
+        return (
+            <div className="settingsForm">
+                <div className='settingsComeBack'>
+                    <Link to="/home" className='settingsLink'>← ПОВЕРНУТИСЬ</Link>
+                </div>
+                <AppBar position="static">
+                    <Tabs value={value} onChange={this.handleChange}>
+                        <Tab label="Профіль" className="settingsTab" />
+                        <Tab label="Шляхи" className="settingsTab" />
+                    </Tabs>
+                </AppBar>
+                {value === 0 && <div>asdasdas</div>}
+                {value === 1 && <div>vbbcbbcccv</div>}
+            </div>
+        );
+    }
 }
+
+
 
 export default userSettingsForm;
