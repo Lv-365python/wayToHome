560962b0db0d9df383c9b2444944eaa02b2b619e
vo333ua <ostapvakh99@gmail.com>
2019-01-12 13:52:02 +0200

    Add message error

--- way_to_home/static/src/components/routeForm/routeForm.js
+++ way_to_home/static/src/components/routeForm/routeForm.js
@@ -3,6 +3,7 @@ import Button from '@material-ui/core/Button';
 import ResultForm from '../routeResult/routeResult';
 import InputPoint from './inputPoint';
 import axios from 'axios';
+import CustomizedSnackbars from '../message/message';
 import './routeForm.css';
 
 
@@ -12,77 +13,84 @@ class RouteSearchForm extends Component {
     state = {
         pointA: undefined,
         pointB: undefined,
-        open: false
+        open: false,
+        error: undefined,
     }
 
-  onClick = () => {
-    this.setState((state) => ({
-        open: !state.open
-    }));
-  }
+    onClick = () => {
+        this.setState((state) => ({
+            open: !state.open
+        }));
+    }
 
-   getCurrentPosition = (props) => {
-      if (!navigator.geolocation){
-        console.log("Geolocation is not supported by your browser")
-        return;
-      }
-      self = this
-      function success(position) {
-        let latitude  = position.coords.latitude;
-        let longitude = position.coords.longitude;
-        let url = 'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat='+latitude+'&lon='+longitude
-         axios.get(url)
-            .then(function (response) {
-                let addr = response.data.address.city + ', ' + response.data.address.road;
-                if (response.data.address.house_number){
-                    addr += ", " + response.data.address.house_number;
-                }
-                props == 'A' ? self.setPointA(addr) :  self.setPointB(addr);
-            })
-            .catch(function (error) {
-                throw error;
-            });
+    getCurrentPosition = (props) => {
+        if (!navigator.geolocation){
+            self.setError("Geolocation is not supported by your browser");
+            return;
         }
-
-      function error() {
-         throw "Unable to retrieve your location";
+        self = this
+        function success(position) {
+            let latitude  = position.coords.latitude;
+            let longitude = position.coords.longitude;
+            let url = 'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat='+latitude+'&lon='+longitude;
+            axios.get(url)
+                .then(function (response) {
+                    const data = response.data.address
+                    let addr = `${data.city || data.town}, ${data.road}`;
+                    if (data.house_number){
+                        addr += `, ${data.house_number}`;
+                    }
+                    props === 'A' ? self.setPointA(addr) :  self.setPointB(addr);
+                })
+                .catch(function (error) {
+                    self.setError(error)
+                });
         }
+        function error() {
+            self.setError("Unable to retrieve your location");
+        }
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
 
-      navigator.geolocation.getCurrentPosition(success, error)
-  }
+    setPointA = (value) => {
+        this.setState({pointA: value})
+    }
 
-  setPointA = (value) => {
-    this.setState({pointA: value})
-  }
+    setPointB = (value) => {
+        this.setState({pointB: value})
+    }
 
-  setPointB = (value) => {
-    this.setState({pointB: value})
-  }
-
-  closeRouteResult = () =>{
-      this.setState({
+    closeRouteResult = () =>{
+        this.setState({
             open: false
-         });
-  }
+        });
+    }
+
+    setError = (value) => {
+        this.setState({
+            error: value
+        });
+    }
 
     render() {
-      const { open } = this.state;
+        const { open, error } = this.state;
 
-      return (
-        <div className='searchForm'>
-          <div style={{marginTop: '50px'}}></div>
-          <InputPoint name='Point A' value={this.state.pointA} onChange={this.setPointA}/>
-          <div style={{marginTop: '50px'}}></div>
-          <InputPoint name='Point B' value={this.state.pointB} onChange={this.setPointB}/>
-          <div style={{marginTop: '60px'}}></div>
-            <Button variant='contained' color='primary' size='medium' onClick={this.onClick} className='Btn'>
-              ПОШУК
-            </Button>
-            <button onClick={this.props.onClose} className="hideBtn">X</button>
-            <button onClick={() => this.getCurrentPosition('A')} className="currPosBtn_1">O</button>
-            <button onClick={() => this.getCurrentPosition('B')} className="currPosBtn_2">O</button>
-            { open && <ResultForm onClose={this.closeRouteResult}/>}
-        </div>
+        return (
+            <div className='searchForm'>
+                <div style={{marginTop: '50px'}}></div>
+                <InputPoint name='Point A' value={this.state.pointA} onChange={this.setPointA}/>
+                <div style={{marginTop: '50px'}}></div>
+                <InputPoint name='Point B' value={this.state.pointB} onChange={this.setPointB}/>
+                <div style={{marginTop: '60px'}}></div>
+                <Button variant='contained' color='primary' size='medium' onClick={this.onClick} className='Btn'>
+                    ПОШУК
+                </Button>
+                { error && <CustomizedSnackbars message={this.state.error} reset={this.setError}/>}
+                <button onClick={this.props.onClose} className="hideBtn">X</button>
+                <button onClick={() => this.getCurrentPosition('A')} className="currPosBtn_1">O</button>
+                <button onClick={() => this.getCurrentPosition('B')} className="currPosBtn_2">O</button>
+                { open && <ResultForm onClose={this.closeRouteResult}/>}
+            </div>
         )
     }
 }
