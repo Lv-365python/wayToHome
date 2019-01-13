a99250e1aa864390a087232e7e10d47a012b685c
Sasha1152 <saxin@ukr.net>
2019-01-13 16:15:14 +0200

    create header and user icon

--- way_to_home/static/src/components/header/header.js
+++ way_to_home/static/src/components/header/header.js
@@ -1,4 +1,9 @@
 import React, {Component} from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
 import LoginBtn from '../loginBtn/loginBtn.js';
 import './header.css'
 import StartBtn from "../startBtn/startBtn.js";
@@ -8,8 +13,14 @@ class Header extends Component{
     render(){
         return(
             <div>
+            <AppBar style={{height: '44px', }} position="static">
+            <Toolbar>
+            WAY TO HOME
                 <StartBtn/>
                 <LoginBtn/>
+                <AccountCircle style={{fontSize: '40px', top: '10px'}}/>
+            </Toolbar>
+            </AppBar>
             </div>
         )
     }
