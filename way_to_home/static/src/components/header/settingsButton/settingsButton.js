3d387931bd30b2dc883d029dcb26608c5b367e0c
Sasha1152 <saxin@ukr.net>
2019-01-16 17:46:48 +0200

    add settingsButton to the header

---
+++ way_to_home/static/src/components/header/settingsButton/settingsButton.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import './settingsButton.css'
+
+
+class SettingsButton extends Component{
+    state = {
+        anchorEl: null,
+    };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+    render(){
+        const {anchorEl} = this.state;
+        return(
+            <div className='SettingsButtonDiv'>
+                <Button
+                    variant="contained"
+                    color='primary'
+                    size='large'
+                    aria-owns={anchorEl ? 'simple-menu' : undefined}
+                    aria-haspopup="true"
+                    onClick={this.handleClick}
+                >
+                   Меню
+                </Button>
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={this.handleClose}
+                >
+                    <MenuItem onClick={this.handleClose}>Мої маршрути</MenuItem>
+                    <MenuItem onClick={this.handleClose}>Мої місця</MenuItem>
+                    <MenuItem onClick={this.handleClose}>Вийти</MenuItem>
+                </Menu>
+            </div>
+        )
+    }
+}
+
+export default SettingsButton;
