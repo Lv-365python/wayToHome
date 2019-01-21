4b0c27f010279b90fea547f3ca165ad4f43ac80f
meelros <meelros91@gmail.com>
2019-01-21 19:46:23 +0200

    hotfix

--- way_to_home/static/src/main_router.js
+++ way_to_home/static/src/main_router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom';
-import {Home, UserSettings} from './components';
+import {Home, UserSettings, Map, Header} from './components';
 
 
 export default class MainRouter extends React.Component {
     render(){
         return (
             <main>
+                <Header/>
+                <Map/>
                 <Switch>
                     <Route path="/home" component={Home}/>
                     <Route path="/settings" component={UserSettings}/>
