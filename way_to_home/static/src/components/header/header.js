b33e3c856385e3f2ca5ab38dc21f9f819988bae5
Sasha1152 <saxin@ukr.net>
2019-01-22 16:33:17 +0200

    PY-233_create a header as a single bar (#44)
    * create header and user icon
    
    * insert buttons as components in header
    
    * change styles header
    
    * create avatar icon fron material ui
    
    * add avatar icon as google image
    
    * add toggle for user icon
    
    * positioning avatar
    
    * use 'react-toggle-display' for show-hide components
    
    * add settingsButton to the header
    
    * add title to the header
    
    * add css for title
    
    * create state for user image
    
    * create route for settings form
    
    * uncomment middleware
    
    * set default avatar
    
    * change inheritance component 'userSettingsForm'
    
    * delete unused import 'HttpResponseRedirect'
    
    * fixed position in css
    
    * rename 'settingsButton' to 'menuButton'
    
    * delete the component 'UserSettings'
    
    * activate isLoggined function
    
    * change font of title
    
    * uncomment line
    
    * add logOut function(UI)
    
    * delete useless import

--- way_to_home/static/src/components/header/header.js
+++ way_to_home/static/src/components/header/header.js
@@ -1,14 +1,57 @@
 import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Avatar from '@material-ui/core/Avatar';
+import ToggleDisplay from 'react-toggle-display';
+
 import LoginBtn from '../loginBtn/loginBtn.js';
-import './header.css';
+import MenuButton from './menuButton/menuButton.js';
+import './header.css'
 
+const style = {
+    top: '0.5%',
+    height: '7%',
+    background: '#4887c1cc',
+    borderRadius: 5,
+};
 
-export default class Header extends Component{
+class Header extends Component {
+
+    state = {
+        show: true,
+    };
+
+    isLoggined = () => {
+        return document.cookie.indexOf('sessionid') !== -1;
+    };
+
     render(){
         return(
             <div>
-                <LoginBtn/>
+            <AppBar style={style}>
+            <Toolbar>
+
+                <div className='Title' onClick={() => this.props.history.push('/home')}>
+                    <p>WayToHome</p>
+                </div>
+
+                <ToggleDisplay show={!this.isLoggined()}>
+                    <LoginBtn />
+                </ToggleDisplay>
+
+                <ToggleDisplay show={this.isLoggined()}>
+                    <AccountCircle className='AvatarDiv' style={{fontSize: '40px'}} />
+                    <MenuButton />
+                </ToggleDisplay>
+
+            </Toolbar>
+            </AppBar>
             </div>
         )
     }
-}
+};
+
+export default withRouter(Header);
