878f7eb1766474c369b159d047b8ad21120b7ebb
vo333ua <ostapvakh99@gmail.com>
2019-01-29 14:23:56 +0200

    Add markers

--- way_to_home/static/src/components/map/map.js
+++ way_to_home/static/src/components/map/map.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import { GOOGLE_MAP_API } from "src/settings"
 import './map.css'
 
 
 export class MapContainer extends Component {
+
+    onMarkerClick = () => {
+
+    };
+
     render() {
         return (
-            <div className='map'>
-                <Map
-                    google={this.props.google}
-                    zoom={14}
-                    initialCenter={{
-                        lat: 49.84,
-                        lng: 24.028667
-                    }}
+            <Map
+                google={this.props.google}
+                zoom={14}
+                className='map'
+                initialCenter={{
+                    lat: 49.84,
+                    lng: 24.028667
+                }}
+            >
+                <Marker
+                    onClick = { this.onMarkerClick }
+                    draggable = {true}
+                    title = { 'Changing Colors Garage' }
+                    position = {{ lat: 49.84, lng: 24.028667 }}
+                    name = { 'Changing Colors Garage' }
                 />
-            </div>
+                <Marker
+                    onClick = { this.onMarkerClick }
+                    draggable = {true}
+                    title = { 'Changing Colors Garage' }
+                    position = {{ lat: 49.84, lng: 24.028667 }}
+                    name = { 'Changing Colors Garage' }
+                />
+            </Map>
         );
     }
 }
