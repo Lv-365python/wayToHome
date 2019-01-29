22f6fd9435ccdc892ec97acdcc21e84c80cf3504
Sasha1152 <saxin@ukr.net>
2019-01-29 22:45:42 +0200

    fix header

--- way_to_home/static/src/components/userSettingsForm/userSettingsForm.js
+++ way_to_home/static/src/components/userSettingsForm/userSettingsForm.js
@@ -2,9 +2,6 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import PropTypes from 'prop-types';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import {withRouter} from 'react-router-dom';
 
 import ProfileTab from './profileTab/profileTab'
 import WayTab from './wayTab/wayTab';
@@ -12,7 +9,7 @@ import PlaceTab from './placeTab/placeTab';
 import './userSettingsForm.css';
 
 
-class UserSettingsForm extends React.Component {
+export default class UserSettingsForm extends React.Component {
     state = {
         value: 0,
     };
@@ -21,17 +18,11 @@ class UserSettingsForm extends React.Component {
         this.setState({ value });
     };
 
-    handleClickAway = () => {
-        this.props.history.push('/home');
-    };
-
     render() {
         const { value } = this.state;
 
         return (
             <div className="settingsForm">
-            <ClickAwayListener onClickAway={this.handleClickAway}>
-            <div>
                 <AppBar position="static">
                     <Tabs value={value} onChange={this.handleChange} centered >
                         <Tab label="Профіль" className="settingsTab" />
@@ -43,10 +34,6 @@ class UserSettingsForm extends React.Component {
                 {value === 1 && <PlaceTab/>}
                 {value === 2 && <WayTab/>}
             </div>
-            </ClickAwayListener>
-            </div>
         );
     }
 }
-
-export default withRouter(UserSettingsForm);
