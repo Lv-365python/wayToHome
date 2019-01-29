42b6d146105096ca115aca66d6be2dba56d1d63d
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2019-01-29 14:07:09 +0200

    Create post delete signal for Notification model

--- way_to_home/notification/apps.py
+++ way_to_home/notification/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class NotificationConfig(AppConfig):
     name = 'notification'
+    label = 'notification'
+
+    def ready(self):
+        import notification.signals
