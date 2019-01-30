e60d0daa04fa3b3a62c6df2d3113e77b45e6f42a
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2019-01-30 13:26:40 +0200

    Create parser for static easy way files

--- way_to_home/utils/easy_way.py
+++ way_to_home/utils/easy_way.py
@@ -1,14 +1,15 @@
-"""
-    This module use for parsing data got in real time from 'Easy Way' source
-    in GTFS format.
-"""
+"""This module provides functionality to work with data from EasyWay."""
+
+from collections import defaultdict
 
 from google import protobuf  # pylint: disable=no-name-in-module, unused-import
 from google.transit import gtfs_realtime_pb2
 
+from .file_handlers import parse_csv_file
+
 
 def compile_file(file_gtfs):
-    """ This function compile gtfs file to json file """
+    """This function compile GTFS file to json file."""
     feed = gtfs_realtime_pb2.FeedMessage()
 
     try:
@@ -24,7 +25,7 @@ def compile_file(file_gtfs):
 
 
 def parse_vehicle_data(feed_entity):
-    """ This function create json file that contain necessary data about certain route """
+    """This function create dictionary that contain necessary data about certain route."""
     vehicle_data = {}
     for entity in feed_entity:
         route_id = entity.vehicle.trip.route_id  # identifier of the certain route
@@ -39,3 +40,52 @@ def parse_vehicle_data(feed_entity):
         })
 
     return vehicle_data
+
+
+def parse_trips_data(file_path='./trips.txt'):
+    """
+    Return data about trips as dictionary where key
+    is id of route and value is list of trips ids.
+    """
+    trips_content = parse_csv_file(file_path, ['route_id', 'trip_id'])
+
+    trips = defaultdict(list)
+    for trip in trips_content:
+        key = trip.get('route_id')
+        value = trip.get('trip_id')
+        trips[key].append(value)
+
+    return trips
+
+
+def parse_routes_data(file_path='./routes.txt'):
+    """
+    Return data about routes as dictionary where key is id
+    of route and value is short name of appropriate route.
+    """
+    routes_content = parse_csv_file(file_path, ['route_id', 'route_short_name'])
+
+    routes = {}
+    for route in routes_content:
+        key = route.get('route_id')
+        value = route.get('route_short_name')
+        routes[key] = value
+
+    return routes
+
+
+def parse_stops_data(file_path='./stops.txt'):
+    """
+    Return data about stops as dictionary where key is id of stop and
+    value is tuple with latitude and longitude of appropriate stop.
+    """
+    stops_content = parse_csv_file(file_path, ['stop_id', 'stop_lat', 'stop_lon'])
+
+    stops = {}
+    for stop in stops_content:
+        key = stop.get('stop_id')
+        latitude = stop.get('stop_lat')
+        longitude = stop.get('stop_lon')
+        stops[key] = (latitude, longitude)
+
+    return stops
