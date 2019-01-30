712dd8863dd92eb276d72d032af6bcada186545f
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2019-01-30 13:30:29 +0200

    Update task for preparing easy way static data
    - Add steps for parsing necessery data from appropriates files
      and insterting it into Redis in pickled representation

--- way_to_home/utils/tasks.py
+++ way_to_home/utils/tasks.py
@@ -1,16 +1,26 @@
 """This module provides celery tasks."""
 
+import pickle
+
 from celery.schedules import crontab
 from celery.task import periodic_task, task
 
 from django.conf import settings
 from notification.models import Notification
 from .file_handlers import load_file, unzip_file
+from .redishelper import REDIS_HELPER as redis
+from .easy_way import parse_routes_data, parse_trips_data, parse_stops_data
 
 
 DEFAULT_RETRY_DELAY = 60
 CLEANER_CTONTAB = crontab(hour=1, minute=30)
 EASYWAY_CTONTAB = crontab(hour=2, day_of_week=1)
+EASYWAY_DIR = settings.EASY_WAY_DIR
+EASYWAY_PARSERS = {
+    'stops': parse_stops_data,
+    'routes': parse_routes_data,
+    'trips': parse_trips_data
+}
 
 
 @periodic_task(bind=True,
@@ -34,17 +44,27 @@ def delete_expired_notifications(self):
                name='prepare static easy way data',
                run_every=EASYWAY_CTONTAB,
                default_retry_delay=DEFAULT_RETRY_DELAY)
-def prepare_static_easy_way_data(self):
-    """Provide preparing static data from EasyWay every Monday at 2 a.m."""
+def prepare_static_easyway_data(self):
+    """
+    Provide preparing static data from EasyWay every Monday at 2 a.m.
+    Defines commands to download static files, unzip it, parse necessary data
+    from appropriate files and insert it into Redis in pickled representation.
+    """
     url = 'http://track.ua-gis.com/gtfs/lviv/static.zip'
 
-    loaded_file = load_file(url, save_to=settings.EASY_WAY_DIR)
+    loaded_file = load_file(url, save_to=EASYWAY_DIR)
     if not loaded_file:
         raise self.retry()
 
-    is_unzipped = unzip_file(loaded_file, unzip_to=settings.EASY_WAY_DIR)
+    is_unzipped = unzip_file(loaded_file, unzip_to=EASYWAY_DIR)
     if not is_unzipped:
         raise self.retry()
+
+    for data_identifier, parser in EASYWAY_PARSERS.items():
+        file_path = f'{EASYWAY_DIR}/{data_identifier}.txt'
+        parsed_data = parser(file_path)
+        pickled_data = pickle.dumps(parsed_data)
+        redis.set(data_identifier, pickle.dumps(pickled_data))
 
 
 @task
