c3b835f4d99a6ed9e3f4882a9033170696228309
Max <mrbrainnygg@gmail.com>
2019-02-01 14:31:13 +0200

    Update telegram bot to save chat id in models, add function to send independent telegram bot message

---
+++ way_to_home/telegram_bot/bothelper.py
@@ -0,0 +1,27 @@
+"""This module provides helping functions for telegram bot."""
+
+import os
+import sys
+import django
+
+# pylint: disable=wrong-import-position
+
+SOURCE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, SOURCE_PATH)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "way_to_home.settings")
+django.setup()
+
+from django.conf import settings
+from telebot import TeleBot
+
+TOKEN = settings.TELEGRAM_BOT_TOKEN
+BOT = TeleBot(token=TOKEN)
+
+
+def send_telegram_message(text, user):
+    """This function sends telegram message with given text to user"""
+    chat_id = user.telegram_id
+    if not chat_id:
+        return False
+    BOT.send_message(chat_id=chat_id, text=text)
+    return True
