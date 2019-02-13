1a6903e543630b85965ba9e3b74cb5c4ae60d787
”Viktoriia <mizin4ik3@gmail.com>
2019-02-13 14:20:49 +0200

    update function create token with expiration time

--- way_to_home/utils/jwthelper.py
+++ way_to_home/utils/jwthelper.py
@@ -2,15 +2,27 @@
 
 import jwt
 from django.conf import settings
+from django.utils import timezone
 
 SECRET_KEY = settings.JWT_KEY
 ALGORITHM = settings.JWT_ALGORITHM
 
 
-def create_token(data):
+def create_token(data, expiration_time=None, not_before_time=None):
     """Function that creates JWT with received date and certain expiration time."""
-    token = jwt.encode(data, SECRET_KEY, ALGORITHM).decode("utf-8")
-    return token
+    try:
+        if expiration_time:
+            exp = int(timezone.now().timestamp()) + expiration_time
+            data['exp'] = exp
+
+        if not_before_time:
+            nbf = int(timezone.now().timestamp()) + not_before_time
+            data['nbf'] = nbf
+
+        token = jwt.encode(data, SECRET_KEY, ALGORITHM).decode("utf-8")
+        return token
+    except TypeError:
+        pass
 
 
 def decode_token(token):
